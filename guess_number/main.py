59a61db19736b89ad4ac56ad412df826879e1082
DeiversonMAP <73710805+DeiversonMAP@users.noreply.github.com>
2022-09-17 15:24:54 -0300

    Pequenas melhorias no código

--- guess_number/main.py
+++ guess_number/main.py
@@ -1,6 +1,6 @@
-import os  # para executar comandos no terminal
 import random  # gerar numero aleatório
-import time  # colocar para dormir
+from os import name, system  # para executar comandos no terminal
+from time import sleep  # colocar para dormir
 
 import colorama as color  # alterar cores dos textos
 
@@ -50,6 +50,12 @@ def number() -> int:
     """
     return random.randint(1, 101)
 
+def clean_screen() -> None:
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
 def verifica_numero(numero: int, count: int = 1) -> None:
     """
     verifica_numero : verifica se o numero chutado bate com o que foi gerado
@@ -60,40 +66,46 @@ def verifica_numero(numero: int, count: int = 1) -> None:
     Keyword Arguments:
         count -- numero de tentativas (default: {1})
     """
-    time.sleep(0.5)
-    if (chute := int(input(f"{green('Qual o número gerado ?')}   ")) ) == numero:
-        time.sleep(1.5)
-        print(f"Parabéns!! Você acerto o numero com {cyan(str(count))} tentativas!\n")
+    sleep(0.5)
+    chute : int
+    if (chute := int(input(f"{green('Qual o número que pensei ?')}   ")) ) == numero:
+        sleep(1.5)
+        print(f"\nParabéns!! Você acertou o número com {cyan(str(count))} tentativas!\n")
     elif 1 > chute or chute > 100 :
-        print(f"Número fora to intervalo entre {red('1')} e {red('100')}, favor chute novamente")
-        verifica_numero(numero=numero, count=count+1)
+        print(f"Número {red(str(chute))} está fora do intervalo entre {red('1')} e {red('100')}, favor chute novamente")
+        verifica_numero(numero=numero, count=count)
     elif chute < numero:
-        print(f"Número {red('menor')} que o esperado, chute novamente um {green('maior')}")
+        print(f"Número {red(str(chute))} é {red('menor')} que o esperado, chute novamente um {green('maior')}")
         verifica_numero(numero=numero, count=count+1)
     else :
-        print(f"Número {red('maior')} que o esperado, chute novamente um {green('menor')}")
-        verifica_numero(numero=numero, count=count)
-
+        print(f"Número {red(str(chute))} é {red('maior')} que o esperado, chute novamente um {green('menor')}")
+        verifica_numero(numero=numero, count=count+1)
 
 def guess() -> None:
     """
     guess : Inicia o jogo
     """
-    print(f"Um numero foi gerado automaticamente entre 1 e 100 ")
+    print(f"Pensei em um número entre entre 1 e 100 ")
     verifica_numero(numero = number())
-    time.sleep(0.5)
+    sleep(0.5)
     if input(f"Deseja jogar novamente? Digite 's' caso sim, qualquer outro valor caso não \n").upper() in ('S','SIM','Y','YES'):
-        time.sleep(0.5)
+        sleep(0.5)
         print(f"{cyan('Reiniciando ...')}")
-        time.sleep(1)
-        os.system("cls")
+        sleep(1)
+        clean_screen()
         guess()
     else:
-        time.sleep(0.5)
-        print(f"{green('Muito obrigado pro jogar conosco!!')}")
-        time.sleep(0.5)
-        print(f"{red('Finalizando...')}")
-        time.sleep(3)
+        sleep(1)
+        print(f"{green('Muito obrigado pro jogar comigo!!')}")
+        sleep(1)
+        print(f"{red('Tchau !!!')}")
+        sleep(3)
+        clean_screen()
 
 if __name__ == '__main__':
+    print(f"""
+{green('#')* 40}
+{green(f"{' Jogo do chute ':#^40}")}
+{green('#')* 40}
+            """)
     guess()
